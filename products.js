d5401bef563819c1bd8df41edb21fdffa16b4391
vignankumar1237-dot <vignankumar1237@gmail.com>
2026-03-27 23:53:56 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,34 +1,19 @@
 const products = [
   {
     id: 1,
-    title: "Nike Air Max Pulse",
+    title: "OWNAIR 2 in 1 Handheld Steam Iron",
     category: "Trending Now",
-    price: "₹12,495",
-    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
-    link: "https://amzn.to/example1"
+    price: "₹3698",
+    image: "images/71jR2weaoAL._SL1500_.jpg",
+    link: "https://amzn.to/4sDajuU"
   },
   {
-    id: 2,
-    title: "Sunset Glow Humidifier",
-    category: "Home Aesthetic",
-    price: "₹1,199",
-    image: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b",
-    link: "https://amzn.to/example2"
+    id: 1,
+    title: "Desidiya® Astronaut Light Projector – 360° Galaxy Star Projector",
+    category: "Limited Deal",
+    price: "₹578",
+    image: "images/71XoNyTuGVL._SL1500_2.jpg",
+    link: "https://amzn.to/4sHYqUE"
   },
-  {
-    id: 3,
-    title: "Wireless Earbuds Pro",
-    category: "Tech",
-    price: "₹2,499",
-    image: "https://images.unsplash.com/photo-1585386959984-a41552231658",
-    link: "https://amzn.to/example3"
-  },
-  {
-    id: 4,
-    title: "Minimal Desk Lamp",
-    category: "Workspace",
-    price: "₹899",
-    image: "https://images.unsplash.com/photo-1507473885765-e6ed057f782c",
-    link: "https://amzn.to/example4"
-  }
+ 
 ];
